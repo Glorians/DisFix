e8a46fb1829602a9642e6429a91c9e5d87796ca9
Alex <sallexx@outlook.com>
2020-04-19 21:30:10 +0300

    laptop work #1

--- DisFix/Service1.cs
+++ DisFix/Service1.cs
@@ -12,9 +12,10 @@ using System.Threading;
 
 namespace DisFix
 {
+    [RunInstaller(true)]
     public partial class Service1 : ServiceBase
     {
-        int SheduleTime = Convert.ToInt32(ConfigurationSettings.AppSettings["ThreadTime"]);
+        //int SheduleTime = Convert.ToInt32(ConfigurationSettings.AppSettings["ThreadTime"]);
         public Thread Worker = null;
 
         public Service1()
@@ -24,9 +25,44 @@ namespace DisFix
 
         protected override void OnStart(string[] args)
         {
-            ThreadStart start = new ThreadStart(Working);
-            Worker = new Thread(start);
-            Worker.Start();
+            try
+            {
+                ThreadStart start = new ThreadStart(Working);
+                Worker = new Thread(start);
+                Worker.Start();
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+
+        public void Working ()
+        {
+            while (true)
+            {
+                try
+                {
+                    Process[] process = Process.GetProcessesByName("Discord");
+
+                    if (process.Length > 0)
+                    {
+                        foreach (Process item in process)
+                        {
+                            item.PriorityClass = ProcessPriorityClass.High;
+                        }
+                    }
+                }
+
+                catch (Exception)
+                {
+                    throw;
+                }
+                Thread.Sleep(1000);
+            }
+            
         }
 
         protected override void OnStop()
